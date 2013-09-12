758789b3eb33a9154a805f3a875b36cc69f73d2e
Jordan <jordanelder10@gmail.com>
2013-09-12 17:43:34 -0500

    First Commit; structured outline.

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<link rel='stylesheet' type='text/css' href='css/index.css' />
+</head>
+<body>
+	
+	<div id='body'>
+		<div id='header'>
+			<div id='banner'>Banner</div>
+		</div>
+		<div id='pages'>Page</div>
+		<div id='footer'>Footer</div>
+	</div>
+
+	<script type='text/javascript' src='js/jquery.js'></script>
+	<script type='text/javascript' src='js/index.js'></script>
+</body>
+</html>
---
+++ vim.bat
@@ -0,0 +1,5 @@
+@echo off
+REM Change following line to your directory
+set wd=C:\xampp\htdocs\photo
+cd C:\Users\jordan
+gvim %wd%\TODO %wd%\css\index.css %wd%\js\index.js %wd%\index.php
